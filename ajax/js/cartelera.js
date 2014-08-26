6559088978928ab1dc6adf0da7ef9b93e7984c2d
Diego Camargo <dicamargov@gmail.com>
2014-08-25 22:44:17 -0500

    mod

--- ajax/js/cartelera.js
+++ ajax/js/cartelera.js
@@ -35,6 +35,8 @@ $.ajax({
 function filterInformation(){
   var clasificaciones = document.getElementsByName("classifArray[]");
   var realClasif = [];
+  var infoToShow = [];
+  var textFilter = document.getElementById("title").value.toUpperCase();
   info = "";
   document.getElementById("cartelera").innerHTML = ("");
   for (var i = 0; i < clasificaciones.length; i++){
@@ -42,7 +44,39 @@ function filterInformation(){
       realClasif.push(clasificaciones[i].value);
     }
   }
-  
+
+  for (var i = 0; i < informationArray.peliculas.length; i++){
+    if ((informationArray.peliculas[i].TITLE.toUpperCase().indexOf(textFilter)>-1)) {
+      alert(informationArray.peliculas[i].TITLE + "Contenido");
+      infoToShow.push(informationArray.peliculas[i]);
+    }
+
+
+
+
+
+
+    info = "<div class='row'><div class='small-4 columns'>"+
+    "<img width='100%' src="+infoToShow[i].IMGPATH+
+    "></img>"+"</div>" + "<div class='small-8 columns'><b>"+
+    infoToShow[i].TITLE+"</b><br>"+
+    "País: "+infoToShow[i].PAIS+
+    "<br>Año: "+infoToShow[i].ANO+
+    "<br>Duración: "+infoToShow[i].DURATION+" min"+
+    "<br>Clasifiación: "+infoToShow[i].CLASIFICACION+
+    "<br>Horarios ";
+    for (var j=0; j < infoToShow[i].horarios.length; j++){
+      info+="<ul>";
+      info+="<li>"+infoToShow[i].horarios[j]+"</li>";
+      info+="</ul>";
+    }
+    info +="</div></div>";
     $("#cartelera").append(info);
+    $("#cartelera").append("<br>");
+
+
+
+    
+  }
 }
     
