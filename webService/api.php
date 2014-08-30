4e9a910b7eabf778e4f0154ec1d858a84de29d58
Diego Camargo <dicamargov@gmail.com>
2014-08-29 22:47:14 -0500

    Modified Stuff

--- webService/api.php
+++ webService/api.php
@@ -2,14 +2,7 @@
 
 	header('Content-Type: application/json');
 	header('Access-Control-Allow-Origin: *');
-	if (isset($_GET["search"])){
-		$search = ($_GET["search"]);
-		$url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f012b5873ca976c2dbc75379fd0dd899&text=".$search."&sort=relevance&format=rest&api_sig=f6d71e54d75d921ccce3c4e73868dcef";	
-	}
-	else{
-		$url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f012b5873ca976c2dbc75379fd0dd899&text=guadalajara&sort=relevance&format=rest&api_sig=f6d71e54d75d921ccce3c4e73868dcef";	
-	}
-	
+	$url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=4916a1cedbcdbc0329455ba90078e987&text=guadalajara&sort=relevance&format=rest&api_sig=daccc72548eb6ecdf9b162b7edee9edc";	
 	
 	// create curl resource 
 	$ch = curl_init();
--- webService/js/read.js
+++ webService/js/read.js
@@ -11,6 +11,7 @@ $.ajax({
         
         informationArray = response;
         photos = informationArray.photos.photo;
+        photos = shuffle(photos);
           for (var i = 0; i < photos.length; i++) {
               //console.log(photos[i]["@attributes"]);
               imgLink = "https://farm"+photos[i]["@attributes"].farm+".staticflickr.com/"+photos[i]["@attributes"].server+"/"+photos[i]["@attributes"].id+"_"+photos[i]["@attributes"].secret+"_b.jpg";
@@ -43,8 +44,9 @@ function changePics(){
   //console.log(pics);
   document.getElementById("pics").innerHTML = "";
   document.getElementById("val").innerHTML = "<center><h3>"+pics+"</h3></center>";
+  photos = shuffle(photos);
   for (var i = 0; i < pics; i++) {
-      console.log(photos[i]["@attributes"]);
+      //console.log(photos[i]["@attributes"]);
       imgLink = "https://farm"+photos[i]["@attributes"].farm+".staticflickr.com/"+photos[i]["@attributes"].server+"/"+photos[i]["@attributes"].id+"_"+photos[i]["@attributes"].secret+"_b.jpg";
       var img = "<img src='"+imgLink+"' width='100%'/>";
       var modal = "<a data-toggle='modal' href='#modal-id"+[i]+"'>"+
@@ -66,5 +68,23 @@ function changePics(){
       $("#pics").append('<br>');
       $("#pics").append(picsHTML);
   }
+}
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
