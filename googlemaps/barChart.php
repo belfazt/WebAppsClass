e7024cb8f3b5c261090144b3fb8a3bf2d488c5b4
Diego Camargo <dicamargov@gmail.com>
2014-09-30 18:39:20 -0500

    Added googlemaps stuff

---
+++ googlemaps/barChart.php
@@ -0,0 +1,40 @@
+<head>
+	<title>Disco BarChart</title>
+</head>
+<body>
+<?php
+//error_reporting(E_ALL);
+//header("Content-type: text/xml");
+$string = file_get_contents("data.json");
+$info = json_decode($string,true);
+$info = $info['data'];
+//print_r ($info);
+function random_color_part() {
+    return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
+}
+
+function random_color() {
+    return random_color_part() . random_color_part() . random_color_part();
+}
+
+
+
+echo "<svg width='100%' height='90%' version='1.1' xmlns='http://www.w3.org/2000/svg'>";
+for ($i=0; $i <count($info) ; $i++) { 
+	$width = 60;
+	$pos = $i*$width;
+	$height = $info[$i]*10;
+	$y = 500 - $height;
+	$color = "#".random_color();
+	echo "<rect x='$pos' y='$y' width='$width' height='$height' style='fill:$color;stroke-width:3;stroke:rgb(0,0,0)' />";	
+}
+echo "</svg>";
+//header('Location: barChart.php');
+
+
+
+?>
+<script type="text/javascript">
+	window.location.reload();
+</script>
+</body>
---
+++ googlemaps/circle.php
@@ -0,0 +1,12 @@
+<?php
+header("Content-type: text/xml");
+echo "<?xml version='1.0' standalone='no'?>
+<!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 
+'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>
+<svg width='100%' height='100%' version='1.1' xmlns='http://www.w3.org/2000/svg'>
+<circle cx='10' cy='10' r='100' stroke='blue' stroke-width='1' fill='black'>
+</circle>
+</svg>";
+
+?>
+
